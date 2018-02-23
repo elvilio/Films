8b3892c2c356434b95ddb131e8da62cdbb57696b
elvilio <elvilio98@gmail.com>
2018-02-23 18:47:59 +0100

    Commit

--- server/client/sfondo.js
+++ server/client/sfondo.js
@@ -1,9 +1,9 @@
 var i = Math.floor((Math.random() * 8) + 1);
 function change_image(){
-	if(i==8) {
+	if(i==9) {
 		i=1;
 	}
-	var img="./photos/w"+i+'.jpg'
+	var img="./photos/w"+i+'@small.jpg'
 	document.getElementById("img1").background=img;
 	i++;
 	setTimeout(change_image, 50000);
