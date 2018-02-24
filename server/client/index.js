8ecad91837bdaecdf90450bef674b14202107f79
elvilio <elvilio98@gmail.com>
2018-02-24 14:31:23 +0100

    Commit

--- server/client/index.js
+++ server/client/index.js
@@ -1,5 +1,5 @@
 const app = new Vue({
-	el: '.cont',
+	el: '#app',
 	data: {
 		films: [],
 		username: '',
--- server/client/sfondo.js
+++ server/client/sfondo.js
@@ -1,12 +1,31 @@
 var i = Math.floor((Math.random() * 8) + 1);
-function change_image(){
-	if(i==9) {
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function change_image(){
+	if(i==8) {
 		i=1;
 	}
+
+	await sleep(2500);
 	var img="url(./photos/w"+i+'@small.jpg)'
-	document.querySelector(".img1").style.backgroundImage=img;
+	document.querySelector(".img2").style.backgroundImage=img;
+	await sleep(2500);
+
+	document.querySelector(".img1").style.opacity=0;
+	await sleep(2500);
+
 	i++;
-	setTimeout(change_image, 50000);
+	img="url(./photos/w"+i+'@small.jpg)'
+	document.querySelector(".img1").style.backgroundImage=img;
+
+	await sleep(2500);
+	document.querySelector(".img1").style.opacity=100;
+
+	await sleep(50000);
+	change_image();
 }
 
 change_image();
