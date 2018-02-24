ce3927bfe7af46200f36c8313366da204eddfc43
elvilio <elvilio98@gmail.com>
2018-02-24 19:51:35 +0100

    Photos!
    Photos!

--- server/client/sfondo.js
+++ server/client/sfondo.js
@@ -5,12 +5,12 @@ function sleep(ms) {
 }
 
 async function change_image(){
-	if(i==8) {
+	if(i==32) {
 		i=1;
 	}
 
 	await sleep(2500);
-	var img="url(./photos/w"+i+'@small.jpg)'
+	var img="url(./photos/w"+i+'.jpg)'
 	document.querySelector(".img2").style.backgroundImage=img;
 	await sleep(2500);
 
@@ -18,7 +18,7 @@ async function change_image(){
 	await sleep(2500);
 
 	i++;
-	img="url(./photos/w"+i+'@small.jpg)'
+	img="url(./photos/w"+i+'.jpg)'
 	document.querySelector(".img1").style.backgroundImage=img;
 
 	await sleep(2500);
