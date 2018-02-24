696982b7e230aab2791a73d5b3542c6d112db280
aziis98 <antonio.delucreziis@gmail.com>
2018-02-24 13:30:11 +0100

    Fixate cose nel JS

--- server/client/login.js
+++ server/client/login.js
@@ -8,18 +8,19 @@ const app = new Vue({
 
 	created() {
 		// request from server at /api/films
-		const mock = {
-			"tt2543164": {
-				"id": "tt2543164",
-				"addedBy": "aziis98",
-			},
-		}
+		// const mock = {
+		// 	"tt2543164": {
+		// 		"id": "tt2543164",
+		// 		"addedBy": "aziis98",
+		// 	},
+		// }
 	},
 
 	methods: {
 		log_in: function (event) {
 			localStorage.setItem('maquindi-films-username', this.username);
-			console.log('fglsnhgfd');
+			console.log('Logged in as "%s"', this.username);
+			location.replace('/');
 		},
 	}
 })
